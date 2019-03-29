40d1c77f625c6663ceee5b8366ad15cb0a0d74c6
chenshuichuan <chenshuichuan@users.noreply.github.com>
2019-03-29 23:03:20 +0800

    2018年验收版本

--- src/main/java/hpcomputeing/tools/Test.java
+++ src/main/java/hpcomputeing/tools/Test.java
@@ -8,8 +8,7 @@ package hpcomputeing.tools;/**
 import hpcomputeing.domain.ReadDataFile;
 import hpcomputeing.entities.Stock;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  *@ClassName: Test
@@ -20,27 +19,68 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
 
-//        List<String>strings = new ArrayList<>();
-//        strings.add("str1");
-//        strings.add("str2");
-//        strings.add("str3");
-//        strings.add("str1");
-//        for (String str:strings){
-//            switch (str){
-//                case "str1":{
-//                    int i =0;
-//                    System.out.println("str1"+i);
-//                    break;}
-//                case "str2":
-//                    System.out.println("str2");
-//                    break;
-//                case "str3":
-//                    System.out.println("str3");
-//                    break;
-//            }
-//        }
 
-        testCase();
+        testOptiona();
+    }
+    public  static void testOptiona(){
+        /* of */
+        //调用工厂方法创建Optional实例
+        Optional<String> name = Optional.of("YanWei");
+        //传入参数为null，抛出NullPointerException.
+        //Optional<String> someNull = Optional.of(null);
+
+        /*opNullable*/
+        Optional empty = Optional.ofNullable(null);
+
+        /*isPresent*/
+        if (name.isPresent()) {
+            System.out.println(name.get());//输出YanWei
+        }
+
+        /*get*/
+        try {
+            System.out.println(empty.get());
+        } catch (NoSuchElementException ex) {
+            System.err.println(ex.getMessage());
+        }
+
+        /*ifPresent*/
+        name.ifPresent((value) -> {
+            System.out.println("The length of the value is: " + value.length());
+        });
+
+        /*orElse*/
+        System.out.println(empty.orElse("There is no value present!"));
+        System.out.println(name.orElse("There is some value!"));
+
+        /*orElseGet*/
+        System.out.println(empty.orElseGet(() -> "Default Value"));
+        System.out.println(name.orElseGet(String::new));
+        /*orElse*/
+        System.out.println(empty.orElse("There is no value present2!"));
+        System.out.println(empty.orElseGet(() -> "Value"));
+        /*orElseThrow*/
+        try {
+            empty.orElseThrow(IllegalArgumentException::new);
+        } catch (Throwable ex) {
+            System.out.println("error:" + ex.getMessage());
+        }
+
+        /*map*/
+        Optional<String> upperName = name.map((value) -> value.toUpperCase());
+        System.out.println(upperName.orElse("No value found"));
+
+        /*flatMap*/
+        upperName = name.flatMap((value) -> Optional.of(value.toUpperCase()));
+        System.out.println(upperName.get());
+
+        /*filter*/
+        List<String> names = Arrays.asList("YanWei","YanTian");
+        for(String s:names)
+        {
+            Optional<String> nameLenLessThan7 = Optional.of(s).filter((value) -> value.length() < 7);
+            System.out.println(nameLenLessThan7.orElse("The name is more than 6 characters"));
+        }
     }
     public  static void testCase(){
         List<Stock> paramList = ReadDataFile.ReadParam("model2");
